9cf509bdca3280335339c48d4b6b2e8920185dba
Sophakabuso <60679360@mylife.unisa.ac.za>
2023-11-12 23:28:51 +0200

    update err hlg

--- src/services/ImageService.js
+++ src/services/ImageService.js
@@ -1,10 +1,20 @@
 import * as ImagePicker from 'expo-image-picker';
 
+/**
+ * Launches the device's camera and captures an image.
+ * @async
+ * @function
+ * @returns {Promise<void>} - A Promise that resolves with the captured image's URI.
+ */
 const captureImage = async () => {
-    let result = await ImagePicker.launchCameraAsync();
+    try {
+        let result = await ImagePicker.launchCameraAsync();
 
-    if (!result.canceled) {
-        console.log(result.uri);
+        if (!result.cancelled) {
+            console.log(result.uri);
+        }
+    } catch (error) {
+        console.log(error);
     }
 };
 
